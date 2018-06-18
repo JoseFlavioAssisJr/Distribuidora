3093d221e8e25a87fc393a98d10121eb25a9e549
Tiago Nogueira <tiagonogueirajf@gmail.com>
2018-06-18 11:25:39 -0300

    Inserido menu nas outras views

--- view/exibirBebidas.php
+++ view/exibirBebidas.php
@@ -21,6 +21,32 @@
 
 	<div class="container-fluid">
 		
+			<div class="row">
+			
+			<div class="row menu">
+				<div class="col-12">
+					<ul class="nav">
+						<li class="nav-item">
+							<a class="nav-link active" href="#">Home</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="inserirBebidas.php">Cadastro</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="controlerBebidas.php?opcao=2">Listar Bebidas</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">Blog</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link " href="#">Contato</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+
+
 		<div class="row">
 			
 			<div class="col-12">
--- view/inserirBebidas.php
+++ view/inserirBebidas.php
@@ -13,53 +13,79 @@
 
 	<div class="container-fluid">
 		
+
 		<div class="row">
 			
-			<div class="col-12">
-				
-				<h1>Inserir Bebidas</h1>
+			<div class="row menu">
+				<div class="col-12">
+					<ul class="nav">
+						<li class="nav-item">
+							<a class="nav-link active" href="#">Home</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="inserirBebidas.php">Cadastro</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="../controlerBebidas.php?opcao=2">Listar Bebidas</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">Blog</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link " href="#">Contato</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
 
-				<form action="../controlerBebidas.php" method="post">
+			<div class="row">
 
-					<input type="hidden" name="opcao" value="1" />
+				<div class="col-12">
 
-					<div class="form-group">
-						<label for="nome">Nome</label>
-						<input type="text" class="form-control" name="nome" id="nome" placeholder="Nome">
-					</div>
-					<div class="form-group">
-						<label for="volume">Volume</label>
-						<input type="text" class="form-control" name="volume" id="volume" placeholder="Volume">
-					</div>
-					<div class="form-group">
-						<label for="preco">Preco</label>
-						<input type="text" class="form-control" name="preco" id="preco" placeholder="Preco">
-					</div>
-					<div class="form-group">
-						<label for="peso">Peso</label>
-						<input type="text" class="form-control" name="peso" id="peso" placeholder="Peso">
-					</div>
-					<div class="form-group">
-						<label for="qtdEstoque">Quantidade em Estoque</label>
-						<input type="text" class="form-control" name="qde_estoque" id="qde_estoque" placeholder="Quantidade em Estoque">
-					</div>
-					<div class="form-group">
-						<label for="fabricante">Fabricante</label>
-						<input type="text" class="form-control" name="fabricante" id="fabricante" placeholder="Fabricante">
-					</div>
+					<h1>Inserir Bebidas</h1>
 
-					 <button type="submit" class="btn btn-primary">Gravar</button>
-				</form>
+					<form action="../controlerBebidas.php" method="post">
 
+						<input type="hidden" name="opcao" value="1" />
+
+						<div class="form-group">
+							<label for="nome">Nome</label>
+							<input type="text" class="form-control" name="nome" id="nome" placeholder="Nome">
+						</div>
+						<div class="form-group">
+							<label for="volume">Volume</label>
+							<input type="text" class="form-control" name="volume" id="volume" placeholder="Volume">
+						</div>
+						<div class="form-group">
+							<label for="preco">Preco</label>
+							<input type="text" class="form-control" name="preco" id="preco" placeholder="Preco">
+						</div>
+						<div class="form-group">
+							<label for="peso">Peso</label>
+							<input type="text" class="form-control" name="peso" id="peso" placeholder="Peso">
+						</div>
+						<div class="form-group">
+							<label for="qtdEstoque">Quantidade em Estoque</label>
+							<input type="text" class="form-control" name="qde_estoque" id="qde_estoque" placeholder="Quantidade em Estoque">
+						</div>
+						<div class="form-group">
+							<label for="fabricante">Fabricante</label>
+							<input type="text" class="form-control" name="fabricante" id="fabricante" placeholder="Fabricante">
+						</div>
+
+						<button type="submit" class="btn btn-primary">Gravar</button>
+					</form>
+
+
+				</div>
 
 			</div>
-
 		</div>
-	</div>
 
 
-	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+		<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+		<script type="text/javascript" src="js/bootstrap.min.js"></script>
 
-</body>
-</html>
+	</body>
+	</html>
